90b069d7707b5d9de32e7f4cfa73037c0f25cbc3
Martin Lütken <martin@lutken.dk>
2021-09-13 14:23:37 +0200

    Split compileflags in C and C++ flags

--- tools/cmake/set_standard_compiler_flags.cmake
+++ tools/cmake/set_standard_compiler_flags.cmake
@@ -1,14 +1,16 @@
 # File to detect compiler and from this set standard and common compile flags.
-# STANDARD_COMPILE_FLAGS: Are intended to be used in most cases
-# COMMON_COMPILE_FLAGS  : Minimun needed flags. Used for example for 3rdparty libraties that
-#                         can't compile without warnings using the standard compile flags
+# STANDARD_C/CXX_COMPILE_FLAGS: Are intended to be used in most cases
+# COMMON_C/CXXCOMPILE_FLAGS   : Minimun needed flags. Used for example for 3rdparty libraties that
+#                               can't compile without warnings using the standard compile flags
 
 # See list of compile IDs here: https://cmake.org/cmake/help/latest/variable/CMAKE_LANG_COMPILER_ID.html#variable:CMAKE_%3CLANG%3E_COMPILER_ID
 
 
 if ( (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU") OR (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang") )
-    set (COMMON_COMPILE_FLAGS "-fPIC")
-    set (STANDARD_COMPILE_FLAGS "-Wall;-Wextra;-Wsign-conversion;-Wno-zero-as-null-pointer-constant;-Werror;-fPIC")
+    set (COMMON_C_COMPILE_FLAGS "-fPIC")
+    set (COMMON_CXX_COMPILE_FLAGS "-fPIC")
+    set (STANDARD_C_COMPILE_FLAGS "-Wall;-Wextra;-Wsign-conversion;-Werror;-fPIC")
+    set (STANDARD_CXX_COMPILE_FLAGS "-Wall;-Wextra;-Wsign-conversion;-Wno-zero-as-null-pointer-constant;-Werror;-fPIC")
 #elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "MSVC")
 #elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "ARMCC")
 #elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "ARMClang")
